f6d7d6e252673d98e8e451a3abf1dac20eb2050d
Lei Wang <wanglei523@gmail.com>
2020-11-23 08:41:13 +0800

    Update STEP1: Matrix_preparation.py

--- Recommendation/Data_preparation/STEP1: Matrix_preparation.py
+++ Recommendation/Data_preparation/STEP1: Matrix_preparation.py
@@ -1,12 +1,12 @@
 import pandas as pd
 rt = pd.read_csv('D:\\sr-kl\\dataset\\ml-out\\rtmatrix.csv')
-# Utility function: Map the value between [0,1]. The smaller the value, the better.
+# Normalizing the value. The smaller the value, the better.
 def ufm(a, amin=0.001, amax=0.349):
         return (amax - a) / (amax - amin)
 
 for i in rt:
     rt[i] = rt[i].map(ufm)
-# Calculate user satisfaction
+# Calculating user satisfaction
 def uf(i):
     if i < 0.5:
         return i * (i + 0.5)
